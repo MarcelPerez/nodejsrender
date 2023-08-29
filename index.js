1b89a476ad740bff413fa822477db28f66b2c399
MarcelPerezF <marcelperezfernandez@gmail.com>
2023-08-29 14:10:02 -0400

    METODO API

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola, esto es una prueba!!");
+  res.send("Holaa, esto es una prueba!!");
+});
+
+app.get("/api/", (req, res) => {
+  res.send("Holaa, este es el metodo api!!");
 });
 
 app.listen(4003);
