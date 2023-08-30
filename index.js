08520c15289fd72024c9768c37e49ce4544fb3c7
MarcelPerezF <marcelperezfernandez@gmail.com>
2023-08-30 09:08:26 -0400

    TESTING

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from "express";
-import { config } from "dotenv";
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const userRouter = require("./users/user.router");
+
+// import { config } from "dotenv";
+
+// config();
 
 app.use(express.json());
-
-config();
-
-const app = express();
 
 app.get("/", (req, res) => {
   res.send("Holaa, esto es una prueba!!");
@@ -15,7 +18,7 @@ app.get("/api/", (req, res) => {
   res.send("Holaa, este es el metodo api!!");
 });
 
-// app.use("/api/Users", userRouter);
+app.use("/api/Users", userRouter);
 
 app.listen(4003);
 console.log("Serve on port", 4003);
--- users/user.router.js
+++ users/user.router.js
@@ -7,7 +7,7 @@ const {
   login,
 } = require("./user.controller");
 const router = require("express").Router();
-const { checkToken } = require("../../auth/token_validation");
+const { checkToken } = require("../auth/token_validation");
 
 router.post("/", checkToken, createUser);
 router.get("/", checkToken, getUsuarios);
